50004c0dedaf2ed5e9d7050832f205e48f633c93
makowishes <alejin2004@gmail.com>
2025-09-27 01:24:46 -0400

    Initial commit

---
+++ src/main/java/com/example/Marketing_Agent/agents/MarketingAgent.java
@@ -0,0 +1,19 @@
+package com.example.Marketing_Agent.agents;
+import com.google.adk.agents.LlmAgent;
+
+public class MarketingAgent {
+
+    public static LlmAgent ROOT_AGENT = LlmAgent.builder()
+            .name("MarketingAgent")
+            .model("gemini-2.0-flash")
+            // A concise summary of the agent's overall purpose. This becomes crucial later
+            // when other agents need to decide whether to delegate tasks to this agent.
+            .description("A helpful marketing agent that helps plan videos for a business ")
+            // Detailed guidance for the LLM on how to behave, its persona, its goals,
+            // and specifically how and when to utilize its assigned `tools`.
+            .instruction("""
+				You are a friendly and enthusiastic Marketing Agent.
+				Your goal is to make sure the user can have make the most up to date marketing content.
+				""")
+            .build();
+}
